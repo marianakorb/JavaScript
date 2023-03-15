52508019befc9885e9e6a5f99b3573e8551d304b
marianakorb <mariana.korb.mk@gmail.com>
2023-03-15 16:03:36 -0300

    contaLetra com filter

--- codewars/contaLetras.js
+++ codewars/contaLetras.js
@@ -1,1 +1,8 @@
 // Conta letras com método filter
+
+function contaLetra(palavra, letra) {
+    let cadaLetra = palavra.split('')
+    let ocorrencias = cadaLetra.filter(str => str === letra )
+    return ocorrencias.length
+}
+console.log(contaLetra('mariana', 'a'))
